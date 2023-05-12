6975bf5265653a2fbbf517dab4e689a2c9969ad7
knmarcin <knapmarcin@icloud.com>
2023-05-12 13:54:44 +0200

    more git cmds

--- after/plugin/git.rc.lua
+++ after/plugin/git.rc.lua
@@ -7,5 +7,12 @@ git.setup({
     blame = "<Leader>gb",
     -- Open file/folder in git repository
     browse = "<Leader>go",
+    -- Open PR
+    open_pull_request = "<Leader>pr",
+    -- Create PR
+    create_pull_request = "<Leader>cpr",
+    -- git diff
+    diff = "<Leader>gd",
+    diff_close = "q",
   }
 })
