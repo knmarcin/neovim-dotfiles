19c9b37f5bbe91a79f4efceaf8148685cdc4d5ca
knmarcin <knapmarcin@icloud.com>
2023-12-10 02:04:54 +0100

    update

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -2,36 +2,36 @@ local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
 bufferline.setup({
-  options = {
-    mode = "tabs",
-    separator_style = 'thin',
-    show_buffer_close_icons = false,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    color_icons = true,
-    show_buffer_name = true,
-    always_show_bufferline = true,
-  },
-  highlights = {
-    separator = {
-      fg = '#073642',
-      bg = '#002b36',
+    options = {
+        mode = "tabs",
+        separator_style = 'slant',
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+        show_tab_indicators = true,
+        color_icons = true,
+        show_buffer_name = true,
+        always_show_bufferline = true,
     },
-    separator_selected = {
-      fg = '#abb2bf',
-    },
-    background = {
-      fg = '#657b83',
-      bg = '#282c34'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-    },
-    fill = {
-      bg = '#282c34'
-    }
-  },
+    -- highlights = {
+    --     -- separator = {
+    --     --     fg = '#002b36',
+    --     --     bg = '#002b36',
+    --     -- },
+    --     separator_selected = {
+    --         fg = '#abb2bf',
+    --     },
+    --     background = {
+    --         fg = '#657b83',
+    --         bg = '#282c34'
+    --     },
+    --     buffer_selected = {
+    --         fg = '#fdf6e3',
+    --         bold = true,
+    --     },
+    --     fill = {
+    --         bg = '#282c34'
+    --     }
+    -- },
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
--- lua/maps.lua
+++ lua/maps.lua
@@ -7,29 +7,31 @@ vim.api.nvim_set_keymap('n', '[c', '<cmd>lua require("gitsigns").prev_hunk()<CR>
 vim.api.nvim_set_var('mapleader', ' ')
 
 -- Tab navigation
-vim.api.nvim_set_keymap('n', '<Leader>1', '1gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>2', '2gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>3', '3gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>4', '4gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>5', '5gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>6', '6gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>7', '7gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>8', '8gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>9', '9gt', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>0', ':tablast<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>1', '1gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>2', '2gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>3', '3gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>4', '4gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>5', '5gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>6', '6gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>7', '7gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>8', '8gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>9', '9gt', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>0', ':tablast<cr>', { noremap = true })
 -- vim.api.nvim_set_keymap('n', '<c-P>', '<cmd>lua require("fzf-lua").live_grep()<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-p>', [[<cmd>lua require'fzf-lua'.live_grep({ winopts = { preview = { layout = 'vertical', vertical = 'up:50%' } } })<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-p>',
+    [[<cmd>lua require'fzf-lua'.live_grep({ winopts = { preview = { layout = 'vertical', vertical = 'up:50%' } } })<CR>]],
+    { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<cr>', { noremap = true })
 
 -- Moving lines
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', {noremap = true})
-vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', {noremap = true})
-vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', {noremap = true})
-vim.api.nvim_set_keymap('v', '<A-j>', ':m \'>+1<CR>gv=gv', {noremap = true})
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', {noremap = true})
-vim.api.nvim_set_keymap('v', '<A-k>', ':m \'<-2<CR>gv=gv', {noremap = true})
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', { noremap = true })
+vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { noremap = true })
+vim.api.nvim_set_keymap('v', '<A-j>', ':m \'>+1<CR>gv=gv', { noremap = true })
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ':m \'<-2<CR>gv=gv', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<C-t>', ':tabnew<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-t>', ':tabnew<CR>', { noremap = true })
 
 -- Key mappings
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {})
-
